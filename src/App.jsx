d663d8a500fc4cb0eba97074b9e8d6ad16f6013f
Rodriques <rodriques.perry@gmail.com>
2025-04-28 18:54:34 -0500

    Update: basic homepage categories scaffolding

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+function App() {
+	return (
+		<>
+			<div className='categories-container'>
+
+				<div className='category-container'>
+					{/* <img  />*/}
+					<h2>Hats</h2>
+					<p>Shop Now</p>
+				</div>
+
+				<div className='category-container'>
+					{/* <img  />*/}
+					<h2>Jackets</h2>
+					<p>Shop Now</p>
+				</div>
+
+				<div className='category-container'>
+					{/* <img  />*/}
+					<h2>Sneakers</h2>
+					<p>Shop Now</p>
+				</div>
+
+				<div className='category-container'>
+					{/* <img  />*/}
+					<h2>Womens</h2>
+					<p>Shop Now</p>
+				</div>
+
+				<div className='category-container'>
+					{/* <img  />*/}
+					<h2>Mens</h2>
+					<p>Shop Now</p>
+				</div>
+        
+			</div>
+		</>
+	);
+}
+
+export default App;
