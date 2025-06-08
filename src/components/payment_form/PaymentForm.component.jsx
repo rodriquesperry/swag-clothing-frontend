459b98fa944451430a4d9348d2b67aef2f470b81
Rodriques <rodriques.perry@gmail.com>
2025-06-08 14:54:07 -0500

    Resolving unseen CardElement

--- src/components/payment_form/PaymentForm.component.jsx
+++ src/components/payment_form/PaymentForm.component.jsx
@@ -68,6 +68,7 @@ const PaymentForm = () => {
 		<div className='payment-form-container'>
 			<form onSubmit={paymentHandler} className='form-container'>
 				<h2>Credit Card Payment:</h2>
+        <h1>This is a test for production!!</h1>
 				<CardElement />
 				{/* <Button
 					children={'Pay Now'}
